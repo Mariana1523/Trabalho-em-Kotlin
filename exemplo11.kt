be0f22e7fb425a92f4a43c74759d522eb269492c
Mariana1523 <79233254+Mariana1523@users.noreply.github.com>
2022-02-06 13:04:02 -0300

    Create exemplo11.kt

---
+++ exemplo11.kt
@@ -0,0 +1,8 @@
+fun main()
+{
+     class Alunas constructor(var idade: Int=20, var nome: String="indefinido") {}
+  
+     var aluna1 = Alunas(20);
+     var aluna2 = Alunas(20);
+     println("Alunas de nome: "+aluna1.nome+" e "+aluna2.nome)
+}

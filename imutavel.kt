5c6e6cc3771006d055697d78a125b39772e96a04
Mariana1523 <79233254+Mariana1523@users.noreply.github.com>
2022-02-06 12:15:59 -0300

    Create imutavel.kt

---
+++ imutavel.kt
@@ -0,0 +1,5 @@
+   val imutavel: Int = 10
+   imutavel = 8;
+ println(imutavel)
+/*O valor não pode ser mudado, pois a variável foi declarada como “val”, portanto esse 
+programa irá gerar um erro*/
